eebf5061ab6b2e9227c9fb38507fe2578d922630
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-02 13:59:07 +0200

    fix: Permissions for products CRUD

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from apps.products.serializers import ProductSerializer
 from apps.products.models import Product
@@ -8,3 +9,8 @@ from apps.products.models import Product
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ('list', 'retrieve'):
+            return [AllowAny()]
+        return [IsAdminUser()]
