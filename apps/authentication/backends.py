c732d2c73760227def41eb4531baf3763ac99534
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-05 10:01:16 +0200

    fix: small refactoring for readability in EmailBackend

--- apps/authentication/backends.py
+++ apps/authentication/backends.py
@@ -8,13 +8,10 @@ class EmailAuthenticationBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
             user = User.objects.get(email=username)
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
         except User.DoesNotExist:
-            User().set_password(password)
             return None
-
-        if user.check_password(password) and self.user_can_authenticate(user):
-            return user
-        return None
 
     def get_user(self, user_id):
         try:
