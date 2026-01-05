decb2e5b3afb0fac1cf4a1193ccea720ebe7e7d0
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-05 12:32:24 +0200

    feat: Product models

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,29 @@
+# Django
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+
+# Local
+from apps.products.models import Product
+
+User = get_user_model()
+
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through="CartProduct")
+    cost = models.DecimalField(MinValueValidator(0), max_digits=10, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        db_table = 'carts'
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, blank=False, on_delete=models.SET_NULL)
+    quantity = models.PositiveIntegerField(MinValueValidator(1))
+
+    class Meta:
+        db_table = 'carts_products'
+        unique_together = ['cart', 'product']
+        ordering = ['product__name']
