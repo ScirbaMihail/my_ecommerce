d508f0c8f0a623582f0b6158d383aa39bcfd7dfc
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-05 17:20:55 +0200

    fix: add products field in CartSerializer

--- apps/cart/serializers.py
+++ apps/cart/serializers.py
@@ -3,9 +3,12 @@ from rest_framework import serializers
 
 # Local
 from apps.cart.models import Cart
+from apps.products.serializers import ProductSerializer
 
 # Define serializer
 class CartSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+
     class Meta:
         model = Cart
         fields = '__all__'
