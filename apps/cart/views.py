cf5e062340ef749677e58d95b4b4a0bed21829e8
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-06 10:03:55 +0200

    feat: /api/carts/{id}/items

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,5 +1,8 @@
 # DRF
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 # Django
 
@@ -12,3 +15,13 @@ from apps.cart.models import Cart
 class CartViewSet(ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+    @action(detail=True, methods=['get'])
+    def items(self, request, pk=None):
+        try:
+            cart = Cart.objects.prefetch_related('products').get(id=pk)
+            data = self.get_serializer(cart).data
+            return Response({'products': data['products'], 'cost': data['cost']}, status=status.HTTP_200_OK)
+        except Cart.DoesNotExist:
+            return Response({'status': 'not found'}, status=status.HTTP_404_NOT_FOUND)
+        
