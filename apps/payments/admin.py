603c64dc370c36d66d36c2d9d130de52bb146de9
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-09 14:18:01 +0200

    feat: Order Models and Admin

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,9 @@
+from unfold.admin import ModelAdmin
 from django.contrib import admin
 
+from apps.payments.models import Order
+
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(ModelAdmin):
+    list_display = ('id', 'user__email', 'cost', 'status')
--- apps/payments/models.py
+++ apps/payments/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+from apps.products.models import Product
+
+User = get_user_model()
+
 
 # Create your models here.
+class Order(models.Model):
+
+    class Statuses(models.TextChoices):
+        PENDING = "pending", _("Pending")
+        PAID = "paid", _("Paid")
+        CANCELED = "canceled", _("Canceled")
+
+    user = models.ForeignKey(User, null=True, blank=False, on_delete=models.SET_NULL)
+    products = models.ManyToManyField(Product, through="OrderProduct")
+    cost = models.PositiveIntegerField()
+    status = models.CharField(choices=Statuses.choices, default=Statuses.PENDING)
+
+
+class OrderProduct(models.Model):
+
+    class Currencies(models.TextChoices):
+        MDL = "mdl", _("MDL")
+        EUR = "eur", _("EUR")
+        USD = "usd", _("USD")
+
+    class Statuses(models.TextChoices):
+        SUCCESS = "success", _("Success")
+        FAIL = "fail", _("Fail")
+
+    order = models.ForeignKey(Order, null=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, blank=False, on_delete=models.SET_NULL)
+    amount = models.PositiveIntegerField()
+    currency = models.CharField(
+        max_length=3, choices=Currencies.choices, default=Currencies.MDL
+    )
+    status = models.CharField(choices=Statuses.choices)
