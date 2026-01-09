075c7834ed03f2837bb1beddac10ca12c73b318d
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-09 14:20:40 +0200

    fix: Now OrderAdmin is readonly

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,9 +1,10 @@
 from unfold.admin import ModelAdmin
 from django.contrib import admin
 
+from apps.core.mixins import ReadOnlyMixin
 from apps.payments.models import Order
 
 # Register your models here.
 @admin.register(Order)
-class OrderAdmin(ModelAdmin):
+class OrderAdmin(ReadOnlyMixin, ModelAdmin):
     list_display = ('id', 'user__email', 'cost', 'status')
