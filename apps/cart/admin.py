7dc9e38499ee83984afec9e104d8b14278d13e20
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-12 17:48:30 +0200

    feat: expandable row in [Order, Cart]Admin

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -3,12 +3,20 @@ from django.contrib import admin
 
 # unfold
 from unfold.admin import ModelAdmin, TabularInline
+from unfold.sections import TableSection
 
 # local
 from apps.cart.models import Cart, CartProduct
 
 
 # Register your models here.
+class ProductTableSection(TableSection):
+    verbose_name = "Products"
+    height = 400
+    related_name = "products"
+    fields = ("name", "description", "price")
+
+
 class CartProductInline(TabularInline):
     model = CartProduct
     extra = 1
@@ -19,5 +27,6 @@ class CartProductInline(TabularInline):
 class CartAdmin(ModelAdmin):
 
     list_display = ("id", "user__email", "cost")
+    list_sections = (ProductTableSection,)
     exclude = ("cost",)
     inlines = [CartProductInline]
--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,5 +1,6 @@
 # unfold
 from unfold.admin import ModelAdmin
+from unfold.sections import TableSection
 
 # django
 from django.contrib import admin
@@ -10,9 +11,19 @@ from apps.core.mixins import ReadOnlyMixin
 
 
 # Register your models here.
+class ProductTableSection(TableSection):
+    verbose_name = "Products"
+    height = 400
+    related_name = "products"
+    fields = ("name", "description", "price")
+
+
 @admin.register(Order)
 class OrderAdmin(ModelAdmin):
     list_display = ("id", "user__email", "cost", "status")
+    list_sections = (
+        ProductTableSection,
+    )
 
 
 @admin.register(Transaction)
