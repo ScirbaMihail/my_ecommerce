218e5588c8d53faaefd4598aa9b033bec041b3ea
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-12 15:28:25 +0200

    feat: TransactionAdmin

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -5,10 +5,16 @@ from unfold.admin import ModelAdmin
 from django.contrib import admin
 
 # local
-from apps.payments.models import Order
+from apps.payments.models import Order, Transaction
+from apps.core.mixins import ReadOnlyMixin
 
 
 # Register your models here.
 @admin.register(Order)
 class OrderAdmin(ModelAdmin):
     list_display = ("id", "user__email", "cost", "status")
+
+
+@admin.register(Transaction)
+class TransactionAdmin(ReadOnlyMixin, ModelAdmin):
+    list_display = ("id", "order__user__email", "amount", "status")
