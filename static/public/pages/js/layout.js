6c7af811817648b6424608496ef371adc302dbfb
Scirba Mihail <mihail.scirba@iongroup.com>
2026-01-22 12:00:17 +0200

    fix: layout header state

--- static/public/pages/js/layout.js
+++ static/public/pages/js/layout.js
@@ -1,18 +1,31 @@
 import { is_logged, logout } from "./api.js"
 
 async function init() {
-    const profileContainer = document.getElementById('header-container__profile')
-    const status = await is_logged()
+    // Read profile container and get auth status
+    const profileContainer = document.getElementById("header-container__profile-container")
+    const logged = await is_logged()
 
-    const loading_elem = profileContainer.querySelector('p')
-    loading_elem.style.display = 'none'
+    // Set display:none for text "Loading ..."
+    const loading_text = profileContainer.querySelector("p")
+    loading_text.style.display = "none"
 
-    const link_elem = profileContainer.querySelector('a#header-container__login-link')
-    link_elem.style.display = 'block'
-    link_elem.innerText = status == true ? "My profile" : "Log in"
+    // Read all buttons
+    const loginBtn = profileContainer.querySelector("a#header-container__login-link")
+    const profileBtn = profileContainer.querySelector("a#header-container__profile-link")
+    const logoutBtnContainer = document.getElementById("header-container__logout-container")
+
+    // Set page for logged user
+    if (logged) {
+        loginBtn.style.display = "none"
+        profileBtn.style.display = "block"
+        logoutBtnContainer.style.display = "block"
+    } else {
+        loginBtn.style.display = "block"
+        profileBtn.style.display = "none"
+        logoutBtnContainer.style.display = "none"
+    }
 
     const logoutBtn = document.getElementById("logout-btn")
-    logoutBtn.style.display = status == true ? "block" : "none"
     logoutBtn.addEventListener("click", (event) => {
         event.preventDefault()
         logout()
